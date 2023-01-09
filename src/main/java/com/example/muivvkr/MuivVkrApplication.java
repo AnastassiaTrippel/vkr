6f0d2872e9fd40bcfce7427d8183875e3ee941fa
Anastassia <a.tire.nastassia@gmail.com>
2023-01-08 22:03:18 -0800

    first commit

---
+++ src/main/java/com/example/muivvkr/MuivVkrApplication.java
@@ -0,0 +1,13 @@
+package com.example.muivvkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuivVkrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuivVkrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/muivvkr/MuivVkrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muivvkr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuivVkrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
